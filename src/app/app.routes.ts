75a4dd24fba4b37086e2692a576cc8e572187522
Przemyslaw Wcislo <pwcislo@ittouch.com>
2021-04-13 23:11:34 +0200

    remove option to register from login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import {LoginComponent} from './login/login.component';
 import {MainComponent} from './main/main.component';
 import {BikesComponent} from "./bikes/bikes-component";
 import {StationsComponent} from "./stations/stations.component";
-import {RegisterComponent} from "./register/register-component";
 import {AuthGuard} from "./common/guards/auth-guard";
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     {
         path: 'start',
