a6272404cc0a8d9ea0ec590eb7b3b34aa1492f24
Mikołaj Wójcik <52234302+WojcikMikolaj@users.noreply.github.com>
2021-05-17 13:07:23 +0200

    kod

---
+++ src/app/common/service/tech.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {CreateTechRequestDTO, TechListDTO, UserDTO} from '../../generated/dto';
+
+@Injectable()
+export class BikeStationService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getAllTechs(): Observable<TechListDTO> {
+        return this.http.get<TechListDTO>("/api/techs");
+    }
+
+
+    addTech(createTechRequestDTO: CreateTechRequestDTO): Observable<any>  {
+        return this.http.post("/api/techs", createTechRequestDTO);
+    }
+
+
+    deleteTech(techId: number): Observable<UserDTO> {
+        return this.http.delete<UserDTO>(`/api/techs/${techId}`);
+    }
+
+}
---
+++ src/app/techs/techs.component.ts
@@ -0,0 +1,127 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {SecurityService} from '../common/service/security.service';
+import {AppService} from '../app.service';
+import {MsgService} from '../common/service/msg.service';
+import {CreateTechRequestDTO, BlockUserRequestDTO, TechListDTO, UserStatus, UserDTO} from '../generated/dto';
+import {UserService} from "../common/service/user.service";
+import {TechService} from "../common/service/tech.service";
+import {map} from 'rxjs/operators';
+
+@Component({
+    selector: 'app-stations',
+    templateUrl: './stations.component.html',
+    styleUrls: ['./stations.component.scss']
+})
+export class StationsComponent implements OnInit {
+
+    constructor(private securityService: SecurityService,
+                private app: AppService,
+                private router: Router,
+                private msgService: MsgService,
+                private userService: UserService,
+                private techService: TechService) {
+    }
+
+    allTechs: UserDTO[];
+    displayedTechs: UserDTO[];
+    userDTO: UserDTO;
+
+    createTechRequestDTO: CreateTechRequestDTO;
+
+
+    showAddTechConfirmDialog: boolean;
+    showDeleteTechConfirmDialog: boolean;
+    showTechStateChangeConfirmationDialog: boolean;
+
+    ngOnInit(): void {
+        this.refreshTechs();
+    }
+
+    refreshTechs() {
+        this.techService.getAllTechs()
+            .subscribe(techs => {
+                this.allTechs = techs.techs;
+                this.displayedTechs = techs.techs;
+            })
+    }
+
+
+    onAddTechClick() {
+        this.createTechRequestDTO = {name: "", password: "****"};
+        this.showAddTechConfirmDialog = true;
+    }
+
+    onAddTechConfirm() {
+        if (this.validateCreateTechRequest(this.createTechRequestDTO)) {
+            this.techService.addTech(this.createTechRequestDTO).subscribe(result => {
+                this.refreshTechs();
+            });
+        }
+        this.showAddTechConfirmDialog = false;
+    }
+
+    onAddTechCancel() {
+        this.createTechRequestDTO = null;
+        this.showAddTechConfirmDialog = false;
+    }
+
+    onSTechChangeStateClick(userDTO: UserDTO) {
+        this.userDTO = userDTO;
+        this.showTechStateChangeConfirmationDialog = true;
+    }
+
+    onTechChangeStateConfirm() {
+        if (this.isTechBlocked(this.userDTO)) {
+            this.userService.unblockUser(this.userDTO.id).subscribe(result => {
+                this.refreshTechs();
+            });
+        } else {
+            this.userService.blockUser(new BlockUserRequestDTO(){id: this.userDTO.id}).subscribe(result => {
+                this.refreshTechs();
+            });
+        }
+
+        this.showTechsStateChangeConfirmationDialog = false;
+    }
+
+    onTechChangeStateCancel() {
+        this.showTechStateChangeConfirmationDialog = false;
+    }
+
+    onDeleteTechClick(userDTO: UserDTO) {
+        this.userDTO = userDTO;
+        this.showDeleteTechConfirmDialog = true;
+    }
+
+    onDeleteTechCancel() {
+        this.showDeleteTechConfirmDialog = false;
+    }
+
+    onDeleteTechConfirm() {
+        this.techService.deleteTech(this.userDTO.id).subscribe(result => {
+            this.refreshTechs();
+            this.msgService.warn(result);
+        });
+
+        this.showDeleteTechConfirmDialog = false;
+    }
+
+    validateCreateTechRequest(createTechRequestDTO: CreateTechRequestDTO): boolean {
+        if (createTechRequestDTO == null) {
+            return false;
+        }
+        if (createTechRequestDTO.name == null || createTechRequestDTO.name.length == 0) {
+            return false;
+        }
+        if (createTechRequestDTO.password == null || createTechRequestDTO.password.length <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    isTechBlocked(s: userDTO) {
+        return s.status === UserStatus.BLOCKED;
+    }
+}
